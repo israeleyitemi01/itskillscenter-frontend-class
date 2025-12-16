2aee264f10381973625f5cf7e6782d8b8b98cebf
israeleyitemi01 <israeleyitemi200@gmail.com>
2025-12-16 13:34:36 +0100

    learnt about js object properties, methods and object display

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -5,8 +5,8 @@
 // fruits[2] = "kiwi";
 
 // Modifying Array Element
-console.log(fruits);
-console.log(fruits[fruit.length - 1]);
+// console.log(fruits);
+// console.log(fruits[fruit.length - 1]);
 // console.log(fruits.length);
 // length is total number of Array (length start from 1)
 // The index of a number is length minus 1(index start from 0)
@@ -18,22 +18,22 @@ console.log(fruits[fruit.length - 1]);
 
 // Array Methods
 // 1. Push - Allows you to add value to the end of an array
-let tasks = ["Wake up ", " 20 Minuites Meditation", " Pray", " Brush my teeth", " Read a book", " Take my bath"];
-console.log(tasks);
-tasks.push(" Eat jollof Rice");
-console.log("push: " + tasks);
+// let tasks = ["Wake up ", " 20 Minuites Meditation", " Pray", " Brush my teeth", " Read a book", " Take my bath"];
+// console.log(tasks);
+// tasks.push(" Eat jollof Rice");
+// console.log("push: " + tasks);
 
 // 2. Shift - Allows you remove value/element from the beginning of an array
-tasks.shift();
-console.log("shift: " + tasks);
+// tasks.shift();
+// console.log("shift: " + tasks);
 
 // 3. Unshift - Allows you to add value to the beginning of an array
-tasks.unshift("Wake up");
-console.log("Unshift: " + tasks);
+// tasks.unshift("Wake up");
+// console.log("Unshift: " + tasks);
 
 // 4. Pop - Allows you to remove value from the end of an array
-tasks.pop();
-console.log("Pop: " + tasks);
+// tasks.pop();
+// console.log("Pop: " + tasks);
 
 
 // NEXT CLASS IS LOOP, AFTER LOOP FUNCTION AND THEN Working with Array Methods: Transforming Arrays with map
@@ -63,13 +63,13 @@ console.log("Pop: " + tasks);
 //     console.log(itSkillsStudents[i]);
 // }
 
-itSkillsStudents.forEach(i => {
-    console.log(i);
-})
+// itSkillsStudents.forEach(i => {
+//     console.log(i);
+// })
 
 // for(let i = 0; i<=100; i++){
 //     console.log(i)
-// }
+// }  
 
 // for (let i=2; i<=30; i +=2){
 //     console.log(i);
@@ -80,10 +80,52 @@ itSkillsStudents.forEach(i => {
 //     console.log(i + 1 + '. ' + fruits[i]);
 // }
 
-let fruit = "Apple"
-let fruits = ["Banana", "Orange", " Mango", fruit, "Pineapple"];
-console.log(fruits);
-fruits[2] = "kiwi";
-fruits.map((fruit, index) => console.log(fruit))
-console.log(index + 1 + ". " + fruit);
+// Array Methods Mapping
+// let fruit = "Apple"
+// let fruits = ["Banana", "Orange", " Mango", fruit, "Pineapple"];
+// fruits.map(function (item, index) {
+//     console.log(index, item);
+// });
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let squares = numbers.map((num) => num * num);
+// console.log(squares);
+
+// filter mehtods
+// let greaterThanFive = numbers.filter((num) => {
+//     return num > 5;
+// });
+// console.log(greaterThanFive);
+
+// let evenNumbers = numbers.filter((even) => {
+//     return even % 2 === 0;
+// })
+// console.log(evenNumbers);
+
+// Reducing Arrays with Reduce
+// let numbs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let sum = numbs.reduce((total, num) => total + num, 0);
+// console.log(sum);
+
+// OR
+// let numbs = [8, 5, 9, 4, 2, 6, 7, 1, 3, 10];
+// let sum = numbs.reduce((accumulators, currentValue) => {
+//     return accumulators + currentValue;
+// }, 0);
+// console.log(sum);
+
+// Finding Elements with find in Arrays
+// let found = numbs.find((num) => num > 6);
+// console.log(found);
+
+// Sorting Arrays 
+// numbs.sort((a, b) => a - b);
+// console.log(numbs);
+
+
+
+// CLASSWORK with reduce
+let fruits = ["Banana ", "Orange ", " Mango ", "Cherry ", "Pawpaw ", "Pineapple ", "Carrot ", "Tomatoe "];
+let sum = fruits.reduce((acc, item, i, arr) => acc +=1, 0);
+console.log(sum);
 
--- javascript/functions.js
+++ javascript/functions.js
@@ -13,17 +13,20 @@
 
 // arguments are the actual values you pass into a function when you call it. They are different from parameters, which are the names listed in the function definition
 
-function division(c, d){
-    let result = c / d;
-    return result
-}
-console.log(division(100, 20));
 
-function modulus(num1, num2){
-    let result = num1 % num2;
-    return result;
-}
-console.log(modulus(80, 3));
+// function division(c, d){
+//     let result = c / d;
+//     return result
+// or
+// return c / d;
+// }
+// console.log(division(100, 5));
+
+// function modulus(num1, num2){
+//     let result = num1 % num2;
+//     return result;
+// }
+// console.log(modulus(80, 3));
 
 // function to greet users
 // function greetUser(name) {
@@ -44,18 +47,27 @@ console.log(modulus(80, 3));
 
 // 3. Arrow Function
 
+// const modulus = (a, b) => a % b
+// console.log(modulus(16, 6));
 // const subtractNumbers = (a, b) => a - b
 // console.log(subtractNumbers(10, 5));
 
 // setTimeout(function() {
-//     alert("I am an anonymous function! called after 5 seconds.")
-// }, 5000);
+//     alert("I am an anonymous function! called after 2 seconds.")
+// }, 2000);
 
 // NOTE: setTimeout is an example of a higher order function
+
 
 // (function() {
 //     alert("This is an immediately invoked function!");
 // })();
+
+function operate(a, b, operation) {
+    return operation(a, b);
+}
+const result = operate(10, 2, (x, y) => x / y);
+console.log(result);
 
 // function operate(a, b, operation) {
 //     return operation(a, b);
@@ -71,6 +83,11 @@ function addNum(a, b, ...numbers) {
 }
 console.log(addNum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11));
 
+// function greet(name = "Guest") {
+//     return `Hello, ${name}!`
+// }
+// console.log(greet());
+
 // Global Scope
 // let geeting = "Hi";
 // function displayGreeting() {
@@ -78,50 +95,60 @@ console.log(addNum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11));
 // }
 // displayGreeting();
 
-// function greet(name){
-//     alert(`${Welcome} + greet(name)`);
+function greet(name){
+    alert(`Welcome, + ${name}!`);
+}
+console.log('Israel Eyitemi');
+
+// const greet = function(name, timeOfDay) {
+//     if(
+//         typeof(timeOfDay) === 'string'
+//         ? (timeOfDay.toLowerCase() === 'morning')
+//         : (timeOfDay <= 11 && timeOfDay >= 0)
+//     )
+//     {
+//         return `Good morning, ${name}!`;
+//     }
+//     else if (
+//         typeof(timeOfDay) === 'string'
+//         ? (timeOfDay.toLowerCase() === 'afternoon')
+//         : (timeOfDay >= 12 && timeOfDay <= 15)
+//     ) {
+//         return `Good afternoon, ${name}!`;
+//     }
+//     else if(
+//         typeof(timeOfDay) === 'string' 
+//         ? (timeOfDay.toLowerCase() === 'evening') 
+//         : (timeOfDay >= 16 && timeOfDay <= 24)
+//      ) { 
+//         return `Good evening, ${name}!`;
+//     }
+//     else {
+//         return 'invalid';
+//     }
 // }
-// console.log(name);
+// console.log(greet('Alice', 'evening'));
 
-const greet = function(name, timeOfDay) {
-    if(
-        typeof(timeOfDay) === 'string'
-        ? (timeOfDay.toLowerCase() === 'morning')
-        : (timeOfDay <= 11 && timeOfDay >= 0)
-    )
-    {
-        return `Good morning, ${name}!`;
-    }
-    else if (
-        typeof(timeOfDay) === 'string'
-        ? (timeOfDay.toLowerCase() === 'afternoon')
-        : (timeOfDay >= 12 && timeOfDay <= 15)
-    ) {
-        return `Good afternoon, ${name}!`;
-    }
-    else if(
-        typeof(timeOfDay) === 'string' 
-        ? (timeOfDay.toLowerCase() === 'evening') 
-        : (timeOfDay >= 16 && timeOfDay <= 24)
-     ) { 
-        return `Good evening, ${name}!`;
-    }
-    else {
-        return 'invalid';
-    }
-}
-console.log(greet('Alice', 'evening'));
+// function calcArea(l, b) {
+//     console.log(l*b)
+// }
+// calcArea(4,5)
 
-function calcArea(l, b) {
-    console.log(l*b)
-}
-calcArea(4,5)
+// function number(num) {
+//     if(num %2 === 0) {
+//         console.log(`${num} is even`)
+//     }else console.log(`${num} is odd`)
+// }
+// number(4)
+// number(7)
 
-function number(num) {
-    if(num %2 === 0) {
-        console.log(`${num} is even`)
-    }else console.log(`${num} is oddd`)
-}
 
-number(4)
-number(7)
+function isEven(number){
+    if(number % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+} 
+console.log(isEven(100));
+console.log(isEven(15));
